b17580d555b644e017572a246e4af70eeb8fb656
Mayank Mohindra <mayankmohindra06@gmail.com>
2020-10-04 19:08:34 +0530

    include produce name logic

--- src/domain.cpp
+++ src/domain.cpp
@@ -1,17 +1,26 @@
 #include "domain.h"
 
+std::vector<order*> domain::possible_suppliers(order &demand, std::vector<order> &supply) {
+    std::vector<order*> suppliers;
+    for (int i = 0; i < supply.size(); i++) {
+        if (demand.price < supply[i].price) {
+            break;
+        }
+        if (supply[i].produce == demand.produce && supply[i].quantity != 0) {
+            suppliers.push_back(&supply[i]);
+        }
+    }
+    return suppliers;
+}
+
+
 void domain::compute_transactions_greedily(std::vector<order> &demand, std::vector<order> &supply, std::vector<Transaction>& transactions) {
     std::sort(demand.begin(), demand.end(), compare_orders_desc);
     std::sort(supply.begin(), supply.end(), compare_orders_asc);
     for (int i = 0;i<demand.size();i++) {
-        for (int j = 0;j<supply.size();j++){
-            if (demand[i].price < supply[j].price) {
-                break;
-            }
-            if (supply[j].quantity == 0) {
-                continue;
-            }
-            Transaction transaction = Transaction(&demand[i], &supply[j]);
+        std::vector<order*> suppliers = possible_suppliers(demand[i], supply);
+        for (int j = 0;j<suppliers.size();j++){
+            Transaction transaction = Transaction(&demand[i], suppliers[j]);
             transactions.push_back(transaction);
             if (demand[i].quantity == 0) {
                 break;
--- src/domain.h
+++ src/domain.h
@@ -7,6 +7,8 @@
 #include "transaction.h"
 
 struct domain {
+    private:
+        static std::vector<order*> possible_suppliers(order &demand, std::vector<order> &supply);
     public:
         static void compute_transactions_greedily(std::vector<order> &demand, std::vector<order> &supply, std::vector<Transaction>& transactions);
 };
